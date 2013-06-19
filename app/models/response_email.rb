30495ece962f8020d1ad6d0c1bb42c994bf7370a
B Alter <barnaby.alter@gmail.com>
2013-06-19 17:56:31 -0400

    Purpose in response email is now unique :bath:

--- app/models/response_email.rb
+++ app/models/response_email.rb
@@ -1,4 +1,5 @@
 class ResponseEmail < ActiveRecord::Base
   attr_accessible :reply_to, :subject, :body, :purpose
   validates_presence_of :reply_to, :subject, :body, :purpose
+  validates_uniqueness_of :purpose
 end
